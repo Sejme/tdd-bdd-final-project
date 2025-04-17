653a45f606f76abbcf91048587d211df8340bc1b
Sejme <graumann.marius@gmail.com>
2025-04-17 05:36:51 -0400

    Done but not tested with behave

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -53,4 +53,6 @@ def step_impl(context):
             "price": row['price'],
             "available": row['available'] in ['True', 'true', '1'],
             "category": row['category']
-        }
+        }        
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,8 +104,30 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
 
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
+@then('I should see "{message}" in the results')
+def step_impl(context, message):
+    element = context.driver.find_element_by_id('search_results')
+    assert(message in celement.text)
+
+@then('I should not see "{message}" in the results')
+def step_impl(context, message):
+    element = context.driver.find_element_by_id('search_results')
+    assert(message not in celement.text)
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
